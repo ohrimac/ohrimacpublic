1eb091df97e2e7ac2c5ee19e2889a43a7f3f7646
ohrimac <mklabouch@ohri.ca>
2025-04-11 12:36:20 -0400

    Add files via upload

---
+++ santaElf.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# This script adds programes to the block list used by Santa for MacOS. Santa must be installed for the script to work.
+#
+# Version 0.0.1
+# Alpha Test
+#
+# Use sanatactl to call Sanata Control. Use Rule to call for a rule and two flags to set what you want to do. --block to block 
+# and --allow to allow. Use --sha256 to block the signing ID of the program/Dev.
+santactl rule --block --sha256 bd0ed4aa414239048b31e3f97a0e103f05232ab4955b57c40b514990885fc33f
+santactl rule --block --sha256 2cc2cf0b8794f80b913bb6189d4b3290c33fc296e23dc5e40b32f8be72b58060
+exit
